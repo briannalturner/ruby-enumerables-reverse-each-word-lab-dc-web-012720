e12d6a8799e2891b5001d38a13555042ec141281
Bri Turner <blturner3527@gmail.com>
2020-01-14 17:09:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence)
   list = sentence.split(' ')
   new_list = list.collect {|a| a.reverse }
   new_list.join(',')
+  new_list
 end
 
 puts reverse_each_word("Hello there, and how are you?")
